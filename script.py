d5c5c460f3152bfd6bbe05fdd51f9625d1768e44
Looper Project Showcases <87170787+LooperShowcase@users.noreply.github.com>
2024-04-10 13:26:19 +0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,15 @@
+def chick_vote ( age ):
+    if age>20:
+        print("true")
+
+    else:
+        print("false")
+    
+def my_sum( n1, n2, n3, n4, n5 ):
+    return n1 + n2 + n3 + n4 + n5 
+     
+
+def my_avg( n1, n2, n3, n4, n5 ):
+    return (n1+n2+n3+n4+n5)/5
+
+print (my_avg(1, 2, 3, 4, 5))     
